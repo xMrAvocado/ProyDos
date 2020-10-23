d874d675f81d8e6a717b3f18ed04a947f0486662
xMrAvocado <73342250+xMrAvocado@users.noreply.github.com>
2020-10-23 14:20:07 +0200

    Versión 0.0.2 Control DNI

--- clientes.py
+++ clientes.py
@@ -1,3 +1,20 @@
-
-class Clients():
+class Clientes():
+    # Clase gestión clientes
     def validarDNI(dni):
+
+        try:
+            tabla = "TRWAGMYFPDXBNJZSQVHLCKE"
+            dig_ext = "XYZ"
+            reemp_dig_ext = {'X': '0', 'Y': '1', 'Z': '2'}
+            numeros = '1234567890'
+            dni = dni.upper()
+            if len(dni) == 9:
+                dig_conrol = dni[8]
+                dni = dni[:8]
+                if dni[0] in dig_ext:
+                    dni = dni.replace(dni[0], reemp_dig_ext[dni[0]])
+                return len(dni == len([n for n in dni if n in numeros])) and tabla[int(dni) % 23] == dig_control
+            return False
+        except:
+            print('Error en el módulo de validacióndel DNI')
+            return None
